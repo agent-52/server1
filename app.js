f9c0b15c6be2170101f77c0a1bd5ba6937caf7c7
agent-52 <abhaybhadauriya52@gmail.com>
2024-08-29 22:53:03 +0530

    make basic server

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.post("/", (req, res) =>{
   res.redirect("/")
 })
 
-const PORT = process.env.PORT || 3000
+const PORT =  3000
 app.listen(PORT, () =>{
   console.log(`server up and running on port${PORT}`)
 })
