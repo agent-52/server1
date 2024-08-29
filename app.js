a28bf7f24af7c63d98dcca789248e095ab49c538
agent-52 <abhaybhadauriya52@gmail.com>
2024-08-30 05:28:30 +0530

    ..

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const app = express()
 
 app.use(express.urlencoded({extended: true}))
 let data = {
-  "name": "abhay"
+  "name": ["abhay", "anshul"]
 };
 
 app.get("/", (req, res) =>{
   
   
-  res.json(data||{})
+  res.json(data)
 })
 
 app.post("/", async (req, res) =>{
   
-   data = await req.body;
-    res.redirect("/")
+  data = await req.body;
+  res.redirect("/")
 })
 
 const PORT =  3000
 app.listen(PORT, () =>{
   console.log(`server up and running on port${PORT}`)
-  console.log("stringified: ",JSON.stringify(data))
+  console.log(data)
 })
 
  
